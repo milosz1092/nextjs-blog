fc02d48e9f5a90ba343dd16bbbf24879d9ab3e8c
Miłosz Szewczyk <milosz.szewczyk@intive.com>
2020-10-08 14:50:00 +0200

    ts support

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps, GetStaticPaths } from 'next';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
@@ -6,7 +7,7 @@ import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>{postData.title}</title>
       </Head>
@@ -21,7 +22,7 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -29,11 +30,11 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
